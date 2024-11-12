043311a8be36b475e873d1d15f397d4f9c3d7cd4
apurbapa <apurba@karmandrones.com>
2024-11-13 00:06:17 +0530

    prepare for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-f#33s4gtedhw8mco6c!sqs%1%0)l2sd=ob6$2$+a(9yr$lrcif
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" #https://saas.prod.railway.app
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
+    
 
 
 # Application definition
